1ee2e6706226417e64f718c7d49b2d3ec850bea8
kjs215 <kjs215@lehigh.edu>
2015-01-28 00:44:06 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////
+//Krishen Shah
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the prgram
+//      javac WelcomeClass.java
+//  run the prgram
+//      java WelcomeClass
+
+//define a class
+public class WelcomeClass {
+
+//add main method
+    public static void main(String[] args){
+        
+// print the statement 
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println(" ^  ^  ^  ^  ^  ^ ");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/\\ ");
+    System.out.println("<-K--J--S--2--1--5->");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println(" v  V  V  V  V  v");
+    System.out.println("This spring break I will be going to Cancun, Mexico");
+    
+    
+    }
+}
